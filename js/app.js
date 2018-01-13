5aaea7e27b6d0e3f726dc288c4f8ffecedcb7ae3
quoc phan <phquoc25@gmail.com>
2018-01-14 00:27:54 +0700

    Implement the necessary functions

--- js/app.js
+++ js/app.js
@@ -1,34 +1,94 @@
-// Enemies our player must avoid
-var Enemy = function() {
+const bugImage = 'images/enemy-bug.png';
+const CELL_WIDTH = 101;
+const CELL_HEIGHT = 83;
+// Game objects
+var GameObject = function(image, x, y) {
+    this.sprite = image;
+    this.x = x;
+    this.y = y;
+}
+
+// Draw the enemy on the screen, required method for game
+GameObject.prototype.render = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
+// Enemies our player must avoid
+var Enemy = function(image, x, y) {
+    GameObject.call(this, image, x, y);
+    this.speed = 10;
+};
+Enemy.prototype = Object.create(GameObject.prototype);
+Enemy.prototype.constructor = Enemy;
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    this.x = this.x >= ctx.canvas.width ? -CELL_WIDTH : this.x + this.speed * dt;
+}
+Enemy.prototype.setSpeed = function(speed) {
+    this.speed = speed;
+}
 
 // Now write your own player class
+Player = function(image, x, y) {
+    GameObject.call(this, image, x, y);
+}
+Player.prototype = Object.create(GameObject.prototype);
+Player.prototype.constructor = Player;
+Player.prototype.update = function() {
+    if (this.x >= ctx.canvas.width) {
+        this.x = ctx.canvas.width - CELL_WIDTH;
+    } else if (this.x <= 0) {
+        this.x = 0;
+    }
+
+    if (this.y >= CELL_HEIGHT * 5) {
+        this.y = CELL_HEIGHT * 5;
+    } else if (this.y <= 0) {
+        this.y = 0
+    }
+}
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+        case 'left':
+            this.x -= CELL_WIDTH;
+            break;
+        case 'right':
+            this.x += CELL_WIDTH;
+            break;
+        case 'up':
+            this.y -= CELL_HEIGHT;
+            break;
+        case 'down':
+            this.y += CELL_HEIGHT;
+            break;
+        default:
+            break;
+    }
+}
 // This class requires an update(), render() and
 // a handleInput() method.
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+allEnemies = [];
+var bugLevel1 = new Enemy(bugImage, -CELL_WIDTH, 0);
+bugLevel1.setSpeed(50);
+var bugLevel2 = new Enemy(bugImage, -CELL_WIDTH, CELL_HEIGHT);
+bugLevel2.setSpeed(50 * 2);
+allEnemies.push(bugLevel1);
+allEnemies.push(bugLevel2);
 // Place the player object in a variable called player
+player = new Player('images/char-boy.png', CELL_WIDTH * 2, CELL_HEIGHT * 5);
 
 
 
